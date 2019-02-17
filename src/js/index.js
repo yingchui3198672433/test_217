210ed06b275ed96cc5aa36e7d807a94de91f3cb8
zhangxy <3198672433@qq.com>
2019-02-17 20:57:42 +0800

    final commit

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,30 @@ var swiper = new Swiper('.banner-list', {
 var scroll = new BScroll('.list', {
     click: true
 });
+
+ajax({
+    url: '/api/list',
+    dataType: 'json',
+    success: function(data) {
+        if (data.code == 1) {
+            render(data.data)
+        }
+    }
+});
+
+
+function render(data) {
+    var list = document.querySelector('.list-cont');
+    list.innerHTML = data.map(function(item) {
+        return `<li>
+                <div class="font">
+                    <p class="title">${item.title}</p>
+                    <p class="info">${item.info}</p>
+                    <p class="date">${item.date}</p>
+                </div>
+                <div class="img">
+                    <img src="${item.url}" alt="">
+                </div>
+            </li>`
+    }).join();
+}
